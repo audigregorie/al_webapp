e56cbf8bbeed57bf0054988a7faea911c444ab06
Audi Young <audi.mycloud@gmail.com>
2024-06-09 17:03:40 +0100

    Feat: Add kiz music to home page

--- src/app/layout/home/home.component.ts
+++ src/app/layout/home/home.component.ts
@@ -5,6 +5,7 @@ import { CommonModule } from '@angular/common'
 import { videoEmbedBaseUrl } from '../../shared/constants/video-embed'
 import { VideoEmbedComponent } from '../../shared/components/video-embed/video-embed.component'
 import { TarraxoVideoService } from '../../shared/services/tarraxo-video.service'
+import { KizMusicService } from '../../shared/services/kiz-music.service'
 
 @Component({
   selector: 'app-home',
@@ -16,13 +17,16 @@ import { TarraxoVideoService } from '../../shared/services/tarraxo-video.service
 export class HomeComponent {
   private urbankizService = inject(UrbankizVideoService)
   private tarraxoService = inject(TarraxoVideoService)
+  private kizMusicService = inject(KizMusicService)
 
   public urbankizVideos = this.urbankizService.urbankizVideoSig
   public tarraxoVideos = this.tarraxoService.tarraxoVideoSig
+  public kizMusic = this.kizMusicService.kizMusicSig
   public videoEmbedBaseUrl = videoEmbedBaseUrl
 
   constructor() {
     this.urbankizService.fetchUrbankizVideos()
     this.tarraxoService.fetchTarraxoVideos()
+    this.kizMusicService.fetchKizMusic()
   }
 }
--- src/app/shared/services/kiz-music.service.ts
+++ src/app/shared/services/kiz-music.service.ts
@@ -1,8 +1,30 @@
-import { Injectable } from '@angular/core'
+import { HttpClient, HttpParams } from '@angular/common/http'
+import { Injectable, inject, signal } from '@angular/core'
+import { PlaylistItem, PlaylistItemResponse } from '../types/dance-video.type'
+import { map, shareReplay } from 'rxjs'
+import { environment } from '../../../environments/environment.development'
 
 @Injectable({
   providedIn: 'root',
 })
 export class KizMusicService {
-  constructor() {}
+  private http = inject(HttpClient)
+
+  private baseUrl = 'https://youtube.googleapis.com/youtube/v3'
+  private playlistItemsUri = '/playlistItems'
+  private baseParam = new HttpParams().set('part', 'snippet').set('playlistId', 'PLFIFv2lqJqFue2hTmZ4dZJ8cKw-87C-f-').set('key', environment.ytApiKey)
+
+  public kizMusicSig = signal<PlaylistItem[]>([])
+
+  constructor() { }
+
+  public fetchKizMusic() {
+    return this.http
+      .get<PlaylistItemResponse>(this.baseUrl + this.playlistItemsUri, { params: this.baseParam })
+      .pipe(
+        map((res: PlaylistItemResponse) => this.kizMusicSig.set(res.items)),
+        shareReplay(1),
+      )
+      .subscribe()
+  }
 }
